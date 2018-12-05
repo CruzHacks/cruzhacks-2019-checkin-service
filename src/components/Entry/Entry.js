51430ff12eabe5de902e0bb4ed9b25fa958ebbe3
mocklee <emockler@ucsc.edu>
2018-12-04 17:16:02 -0800

    added localhost https & hacker emaillookup option

--- src/components/Entry/Entry.js
+++ src/components/Entry/Entry.js
@@ -3,11 +3,16 @@ import logo from 'assets/logo.svg';
 
 import QrReader from "react-qr-reader";
 
+const reader = {
+    border: "100px solid rgba(0, 0, 0, 0.3)",
+  };
+
 class Entry extends Component {
   constructor(props) {
     super(props);
     this.state = {
       delay: 300,
+      resolution: 300,
       result: "No result"
     };
     this.handleScan = this.handleScan.bind(this);
@@ -28,17 +33,20 @@ class Entry extends Component {
   render() {
     return (
       <div className="entry">
+      <img src={logo} alt="logo" className="entry__logo"/>
         <h1 className="entry__title">Scan hacker's QR code!</h1>
-        <img src={logo} alt="logo" className="entry__logo"/>
           <QrReader
             delay={this.state.delay}
+            resolution={this.state.resolution}
             onError={this.handleError}
             onScan={this.handleScan}
+            //style={reader}
             className="entry__qrReader"
-            
-            
           />
           <h1 className="entry__qrReader__title">{this.state.result}</h1>
+          <form>
+            <input className="entry__email" placeholder="Or enter hacker email..."></input>
+          </form>
       </div>
     );
   }
